6c1e8cbbe58b4551bb896e02a7bc1ff4c129675f
Tzu Yu Tai <ttai3@uwo.ca>
2014-09-23 23:39:55 -0400

    updated functions

--- main.cpp
+++ main.cpp
@@ -8,32 +8,48 @@
 
 #include <iostream>
 
-class account{
+class Account{
 public:
     void deposit(int);
+    void setBalance(int);
     bool withdraw(int);
     int balancechk();
-    account();
-    account(int);
-    bool status;
+    Account();
+    Account(int);
+    int status();
+
     
 private:
     //std::string username;
     //std::string password;
     int balance;
 };
-account::account(int balanceset){
+Account::Account(int balanceset){ //set the balance it is the constructor
     balance = balanceset;
 }
-account::account(){
+Account::Account(){
     
 }
-void account::deposit(int depositcash){
+void Account::setBalance(int balanceAmount){
+    balance = balanceAmount;
+}
+int Account::status(){
+    if (balance > 0){
+        return 3; //account open
+    }
+    else if (balance == 0){
+        return 2; //account can be closed
+    }
+    else{
+        return 1; //account is closed
+    }
+}
+void Account::deposit(int depositcash){
     balance = balance + depositcash;
     
     
 }
-bool account::withdraw(int withdrawcash){
+bool Account::withdraw(int withdrawcash){
     if (withdrawcash > balance){
         std::cout<<"Error: Insufficient funds, Withdrawing terminated. Your balance is: ";
         return 0;
@@ -45,36 +61,49 @@ bool account::withdraw(int withdrawcash){
     }
     
 }
-int account::balancechk(){
+int Account::balancechk(){
     return balance;
 }
 
-class chequing : public account{
+class Chequing : public Account{
 
 };
-class saving : public account{
+class Saving : public Account{
 
 };
-class client{
+class Client{
 public:
     void depositSaving(int);
     void chequingToSaving(int);
     void savingToChequing(int);
-    client(int);
+    Client(int);
     int balanceSavingChk();
     int balanceChequingChk();
+    void closeAccount(Account);
 private:
-    saving savingAccount;
-    chequing chequingAccount;
+    Saving savingAccount;
+    Chequing chequingAccount;
 };
 
-int client::balanceSavingChk(){
+int Client::balanceSavingChk(){
     return savingAccount.balancechk();
 }
-int client::balanceChequingChk(){
+int Client::balanceChequingChk(){
     return chequingAccount.balancechk();
 }
-void client::chequingToSaving(int transfer){
+void closeAccount(Account accountToClose){
+    if (accountToClose.status() == 2){
+        accountToClose.setBalance(-1);
+        std::cout<<"Account is closed"<<std::endl;
+    }
+    else if (accountToClose.status()==3){
+        std::cout<<"Account cannot be close as the balance is not 0"<<std::endl;
+    }
+    else{
+        std::cout<<"Account cannot be closed as it is already closed"<<std::endl;
+    }
+}
+void Client::chequingToSaving(int transfer){
     if(chequingAccount.withdraw(transfer)==1){
         savingAccount.deposit(transfer);
     }
@@ -82,7 +111,7 @@ void client::chequingToSaving(int transfer){
         std::cout<<"Cannot transfer due to insufficient funds in chequing";
     }
 }
-void client::savingToChequing(int transfer){
+void Client::savingToChequing(int transfer){
     if(savingAccount.withdraw(transfer)==1){
         chequingAccount.deposit(transfer);
     }
@@ -90,11 +119,11 @@ void client::savingToChequing(int transfer){
         std::cout<<"Cannot transfer due to insufficient funds in savings";
     }
 }
-void client::depositSaving(int moneysavings){
+void Client::depositSaving(int moneysavings){
     savingAccount.deposit(moneysavings);
     
 }
-client::client(int startingcash){
+Client::Client(int startingcash){
     savingAccount.deposit(startingcash);
     chequingAccount.deposit(0);
 }
@@ -102,7 +131,7 @@ int main(int argc, const char * argv[])
 {
     // login for permission
    
-    client albert(5090);
+    Client albert(5090);
     std::cout<<albert.balanceSavingChk()<<std::endl;
     albert.depositSaving(8);
     std::cout<<albert.balanceSavingChk()<<std::endl;
