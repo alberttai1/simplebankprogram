4fe8059959266d2cf96daa267d7159c1a87c7142
Umar Azhar <umar.azhar5@gmail.com>
2014-09-24 02:19:55 -0400

    Account class and new main. fixed issue of imporoper account balance

---
+++ Account.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Account{
+
+	int balance;
+
+public:
+	Account();
+	Account(int);
+	void withdraw(int);
+	void deposit(int);
+	int getBalance();
+};
+
+Account::Account() {
+	balance = 0;
+}
+
+Account::Account(int amount) {
+	balance = amount;
+}
+
+void Account::withdraw(int amount) {
+	balance -= amount;
+}
+
+void Account::deposit(int amount) {
+	balance += amount;
+}
+
+int Account::getBalance() {
+	return balance;
+}
--- main.cpp
+++ main.cpp
@@ -1,115 +1,18 @@
-//
-//  main.cpp
-//  cs3307asn1
-//
-//  Created by Albert on 2014-09-08.
-//  Copyright (c) 2014 ttai3. All rights reserved.
-//
-
 #include <iostream>
+#include "account.cpp"
 
-class account{
-public:
-    void deposit(int);
-    bool withdraw(int);
-    int balancechk();
-    account();
-    account(int);
-    bool status;
-    
-private:
-    //std::string username;
-    //std::string password;
-    int balance;
-};
-account::account(int balanceset){
-    balance = balanceset;
-}
-account::account(){
-    
-}
-void account::deposit(int depositcash){
-    balance = balance + depositcash;
-    
-    
-}
-bool account::withdraw(int withdrawcash){
-    if (withdrawcash > balance){
-        std::cout<<"Error: Insufficient funds, Withdrawing terminated. Your balance is: ";
-        return 0;
-    }
-    else{
-        balance = balance - withdrawcash;
-        return 1;
-        
-    }
-    
-}
-int account::balancechk(){
-    return balance;
-}
+using namespace std;
 
-class chequing : public account{
+int main() {
 
-};
-class saving : public account{
+    Account umar(100);
+    cout << umar.getBalance() << "\n";
 
-};
-class client{
-public:
-    void depositSaving(int);
-    void chequingToSaving(int);
-    void savingToChequing(int);
-    client(int);
-    int balanceSavingChk();
-    int balanceChequingChk();
-private:
-    saving savingAccount;
-    chequing chequingAccount;
-};
+    umar.withdraw(10);
+    cout << umar.getBalance() << "\n";
 
-int client::balanceSavingChk(){
-    return savingAccount.balancechk();
-}
-int client::balanceChequingChk(){
-    return chequingAccount.balancechk();
-}
-void client::chequingToSaving(int transfer){
-    if(chequingAccount.withdraw(transfer)==1){
-        savingAccount.deposit(transfer);
-    }
-    else{
-        std::cout<<"Cannot transfer due to insufficient funds in chequing";
-    }
-}
-void client::savingToChequing(int transfer){
-    if(savingAccount.withdraw(transfer)==1){
-        chequingAccount.deposit(transfer);
-    }
-    else{
-        std::cout<<"Cannot transfer due to insufficient funds in savings";
-    }
-}
-void client::depositSaving(int moneysavings){
-    savingAccount.deposit(moneysavings);
-    
-}
-client::client(int startingcash){
-    savingAccount.deposit(startingcash);
-    chequingAccount.deposit(0);
-}
-int main(int argc, const char * argv[])
-{
-    // login for permission
-   
-    client albert(5090);
-    std::cout<<albert.balanceSavingChk()<<std::endl;
-    albert.depositSaving(8);
-    std::cout<<albert.balanceSavingChk()<<std::endl;
-    albert.savingToChequing(500);
-    std::cout<<albert.balanceSavingChk()<<std::endl;
-    std::cout<<albert.balanceChequingChk()<<std::endl;
-    
+    umar.deposit(200);
+    cout << umar.getBalance() << "\n";
+
     return 0;
 }
-
