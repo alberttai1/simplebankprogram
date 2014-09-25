6ecce03f2a076e6bf71c662c0f9261bc8c3f757e
Tzu Yu Tai <ttai3@uwo.ca>
2014-09-25 01:28:36 -0400

    worked on the commenting

--- Account.cpp
+++ Account.cpp
@@ -2,22 +2,22 @@
 
 class Account{
 protected:
-	int balance;
+	int balance; 
 
 public:
-	Account();
-	Account(int);
-	void withdraw(int);
-	void deposit(int);
-	int getBalance();
+	Account(); //constructor without parameters 
+	Account(int); //constructor with parameter of starting balance 
+	void withdraw(int); //withdraw method 
+	void deposit(int); //deposit method 
+	int getBalance(); //get the balance and return it 
 };
 
-Account::Account() : balance(0) {}
+Account::Account() : balance(0) {} //initalize the balance to zero 
 
-Account::Account(int amount) : balance(amount) {}
+Account::Account(int amount) : balance(amount) {} //constructor initalize the balance to same amount as parameter you set 
 
-void Account::withdraw(int amount) {balance -= amount;}
+void Account::withdraw(int amount) {balance -= amount;} //withdraw amount and set balance equal after subtracting 
 
-void Account::deposit(int amount) {balance += amount;}
+void Account::deposit(int amount) {balance += amount;} //deposit amount and set balance equal after plusing 
 
-int Account::getBalance() {return balance;}
+int Account::getBalance() {return balance;} //return balnace of it
