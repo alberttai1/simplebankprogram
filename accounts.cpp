4b4fd6b12388940437f94d97cbfc43aea2cdde13
Umar Azhar <umar.azhar5@gmail.com>
2014-09-25 01:48:09 -0400

    comments

--- accounts.cpp
+++ accounts.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 //----------------------------------------------------------------------------------------
-class Chequing: public Account {
+class Chequing: public Account { //chequing account has access to all variables and functions of the Account class.
 
 public:
-	Chequing(int amount): Account(amount) {}
+	Chequing(int amount): Account(amount) {} //initializes the chequing account with the specified param.
 
 };
 //----------------------------------------------------------------------------------------
@@ -15,19 +15,19 @@ public:
 
 
 //----------------------------------------------------------------------------------------
-class Saving: public Account {
+class Saving: public Account { //saving account has access to all variables and functions of the account class.
 	double interest;
 
-public:
-	Saving(int amount);
-	Saving(int, double);
+public:	//declares all public methods.
+	Saving(int amount); //initializes savings account with the specified param
+	Saving(int, double); //initializes savings account with the spcified amount and interest.
 	double getInterest();
 
 };
 
-Saving::Saving(int amount): Account(amount) {}
+Saving::Saving(int amount): Account(amount) {} //constructor.
 
-Saving::Saving(int amount, double i) {
+Saving::Saving(int amount, double i) { //constructor
 	balance = amount;
 	interest = i;
 }
